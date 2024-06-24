b36e0a48b75b1ab44a21e6d13a58862171f46b4a
Bojan <bojanche@gmail.com>
2024-06-24 23:24:08 +0200

    Added docs for SystemUiModel

--- composeApp/src/commonMain/kotlin/screens/gamelistscreen/data/SystemUiModel.kt
+++ composeApp/src/commonMain/kotlin/screens/gamelistscreen/data/SystemUiModel.kt
@@ -3,6 +3,9 @@ package screens.gamelistscreen.data
 import com.bojan.gamelistmanager.gamelistprovider.domain.data.RetroArchCoreInfo
 import java.io.File
 
+/**
+ * UI Model for the System.
+ */
 data class SystemUiModel (
     val name: String,
     val systemPath: File,
