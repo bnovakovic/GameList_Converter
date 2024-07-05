7e32f0cb6525cb42320ec48f66f46667c6fa7c24
Bojan <bojanche@gmail.com>
2024-07-05 23:56:15 +0200

    Reverted test change

--- modules/ListExporter/src/commonTest/kotlin/testutils/TestGameListData.kt
+++ modules/ListExporter/src/commonTest/kotlin/testutils/TestGameListData.kt
@@ -6,12 +6,12 @@ import com.bojan.gamelistmanager.listexplorer.domain.data.ExportGameListData
 val testGameListData = ExportGameListData(
     games = listOf(
         ExportGameData(
-            path = "1869 (AGA).zip",
+            path = ".\\1869 (AGA).zip",
             name = "1869 - Erlebte Geschichte Teil I",
             hidden = false,
         ),
         ExportGameData(
-            path = "Action Cat (AGA).zip",
+            path = ".\\Action Cat (AGA).zip",
             name = "Action Cat",
             hidden = true,
         )
